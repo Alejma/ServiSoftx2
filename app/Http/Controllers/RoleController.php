a5582476ef081e7d541109f79495dfc7b3caba95
Alejandro Marín Hoyos <alejma7@gmail.com>
2018-05-31 18:12:24 -0500

    Se agrego el modulo roles(Presenta un error en la asignacion de permisos para ver los paquetes)

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,18 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Caffeinated\Shinobi\Models\Role;;
+use Caffeinated\Shinobi\Models\Permission;
 use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
+/*     * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+       $roles = Role::paginate();
+
+       return view('roles.index', compact('roles', 'roles'));
     }
 
     /**
@@ -23,7 +26,8 @@ class RoleController extends Controller
      */
     public function create()
     {
-        //
+        $permissions = Permission::get();
+        return view('roles.create', compact('permissions'));
     }
 
     /**
@@ -34,50 +38,67 @@ class RoleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      $role = Role::create($request->all());
+
+      //Actualizar permisos
+      $role->permissions()->sync($request->get('permissions'));
+
+      return redirect()->route('roles.index', $role->id)
+          ->with('info', 'Rol guardado con éxito');
     }
+
+
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Role  $role
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Role $role)
     {
-        //
+        return view('roles.show', compact('role'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Role  $role
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Role $role)
     {
-        //
+        $permissions = Permission::get();
+        return view('roles.edit', compact('role', 'permissions'));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Role  $role
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Role $role)
     {
-        //
+        //Actualizar Role
+        $role->update($request->all());
+
+
+        //Actualizar permisos
+        $role->permissions()->sync($request->get('permissions'));
+
+        return redirect()->route('roles.index', $role->id)
+             ->with('info', 'Role actualizado con éxito');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Role  $role
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Role $role)
     {
         //
     }
